8433d845914f2246ab4dc24ebdaa40c7ecb03ecd
RUAN.WAGNER <RUAN.WAGNER@192.168.56.1>
2019-10-03 20:06:19 -0300

    commit inicial

---
+++ src/modelos/Candidato.java
@@ -0,0 +1,45 @@
+package modelos;
+
+public class Candidato {
+    
+    private int id;
+    private String nome;
+    private int id_bairro;
+    private String data_nasc;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId_bairro() {
+        return id_bairro;
+    }
+
+    public void setId_bairro(int id_bairro) {
+        this.id_bairro = id_bairro;
+    }
+
+    public String getData_nasc() {
+        return data_nasc;
+    }
+
+    public void setData_nasc(String data_nasc) {
+        this.data_nasc = data_nasc;
+    }
+    
+    
+    
+    
+}
